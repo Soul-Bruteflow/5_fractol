7220657087a99aed2760d4a3b27b8b90f107007c
bruteflow <bruteflow@iMac-bruteflow.local>
2017-04-26 22:25:04 +0300

    0.0.6

--- src/frac_core_mouse.c
+++ src/frac_core_mouse.c
@@ -11,13 +11,49 @@ int		track_mouse(int x, int y, t_frct *frct)
 	return (0);
 }
 
-void	frac_zoom_in(t_frct *frct)
+void	frac_zoom_in(t_frct *frct, int x, int y)
 {
+	int ad_x = 0;
+	int ad_y = 0;
+	float percentage_x = 0;
+	float percentage_y = 0;
+	if (x < 750)
+	{
+		ad_x = 750 - x;
+		percentage_x = (float)ad_x * (float)100 / (float)frct->mlx->wcenx;
+	}
+	if (x > 750)
+	{
+		ad_x = x - 750;
+		percentage_x = (float)ad_x * (float)100 / (float)frct->mlx->wcenx;
+	}
+	if (y < 450)
+	{
+		ad_y = 450 - y;
+		percentage_y = (float)ad_y * (float)100 / (float)frct->mlx->wceny;
+	}
+	if (y > 450)
+	{
+		ad_y = y - 450;
+		percentage_y = (float)ad_y * (float)100 / (float)frct->mlx->wceny;
+	}
+	percentage_x = (float)(1.5 / frct->jul->zoom) * percentage_x / 100.0f;
+	percentage_y = (float)(1.0 / frct->jul->zoom) * percentage_y / 100.0f;
+	if (x < frct->mlx->wcenx && y < frct->mlx->wceny)
+	{
+		frct->jul->moveX -= percentage_x;
+		frct->jul->moveY -= percentage_y;
+	}
+	if (x > frct->mlx->wcenx && y > frct->mlx->wceny)
+	{
+		frct->jul->moveX += percentage_x;
+		frct->jul->moveY += percentage_y;
+	}
 	frct->jul->zoom *= 1.090;
 	frac_redraw(frct);
 }
 
-void	frac_zoom_out(t_frct *frct)
+void	frac_zoom_out(t_frct *frct, int x, int y)
 {
 	frct->jul->zoom /= 1.090;
 	frac_redraw(frct);
@@ -38,13 +74,14 @@ int	frac_core_mouse(int button, int x, int y, t_frct *frct)
 			frac_redraw_ui(frct);
 		}
 	}
-	if (button == 4)
+	if (button == 3)
 	{
-		frac_zoom_in(frct);
+		printf("1\n");
+		frac_zoom_in(frct, x , y);
 	}
 	if (button == 5)
 	{
-		frac_zoom_out(frct);
+		frac_zoom_out(frct, x , y);
 	}
 	return (0);
 }
